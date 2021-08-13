ea62625ae00899ee2b46d610443723b8d664251e
Amanda <kagakufact@yahoo.co.jp>
2021-08-13 13:11:59 +0900

    #2 googleで検索ワードを入力して検索。chromeを閉じる

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
 from selenium import webdriver
 
 driver = webdriver.Chrome("./chromedriver.exe") #Chromeを動かすドライバを読み込み
-driver.get("https://google.co.jp")
+driver.get("https://sfc.jp/ie/contact/inquiry/home/form.php")
+
+element = driver.find_element_by_name("q")
+element.send_keys("Selenium WebDriver")
+element.submit()
+
+driver.quit()
