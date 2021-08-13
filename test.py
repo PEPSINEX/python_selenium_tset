5d5674e9369c947e786f16ec2b0d8010b4872036
Amanda <kagakufact@yahoo.co.jp>
2021-08-13 14:02:21 +0900

    #2 テスト対象ページの遷移ボタンをクリック

--- test.py
+++ test.py
@@ -3,8 +3,7 @@ from selenium import webdriver
 driver = webdriver.Chrome("./chromedriver.exe") #Chromeを動かすドライバを読み込み
 driver.get("https://sfc.jp/ie/contact/inquiry/home/form.php")
 
-element = driver.find_element_by_name("q")
-element.send_keys("Selenium WebDriver")
-element.submit()
+driver.find_elements_by_css_selector(".submitWrapper")[0].click()
 
-driver.quit()
+driver.execute_async_script("window.setTimeout(arguments[arguments.length - 1], 3000);")
+driver.quit()   
